cc6b1871f07326b18924d92f21fa837b97748fbc
a.nabati <alinabati93@gmail.com>
2020-10-04 16:14:09 +0330

    binding data - placeDetailsPage

--- src/app/pages/place-details/place-details.page.ts
+++ src/app/pages/place-details/place-details.page.ts
@@ -1,6 +1,7 @@
 import { PlaceHealthDetailsComponent } from './../../shared-components/place-health-details/place-health-details.component';
 import { Component, OnInit } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
+import { Place } from 'src/app/interfaces/place';
 
 @Component({
   selector: 'app-place-details',
@@ -8,6 +9,32 @@ import { PopoverController } from '@ionic/angular';
   styleUrls: ['./place-details.page.scss'],
 })
 export class PlaceDetailsPage implements OnInit {
+  center = { lat: 35.758419, lng: 51.404115 };
+  markerPosition = { lat: 35.758419, lng: 51.404115 };
+  markerOptions = { draggable: false };
+
+  place: Place = {
+    id: 1,
+    title: 'بانک ملی شعبه چهارباغ',
+    adderss: '',
+    tel: '',
+    placeHistory: [
+      {
+        id: 1, mask: 98, airConditioning: 70, glove: 40, sanitizer: 68, socialDistanding: 85,
+        _userName: 'علی',
+        comment: 'متاسفانه مواد ضدعفونی به مقدار کافی وجود نداشت.',
+      },
+      {
+        id: 1, mask: 98, airConditioning: 10, glove: 40, sanitizer: 68, socialDistanding: 85,
+        _userName: 'حسین',
+        comment: 'وضعیت استفاده از ماسک بسیار عالی بود و همه افراد از ماسک استفاده می کردند.'
+      },
+      { id: 1, mask: 98, airConditioning: 10, glove: 40, sanitizer: 68, socialDistanding: 85,
+      _userName:'سارا',
+      comment:'تهویه هوا واقعا بد بود.'
+      },
+    ]
+  }
 
   segValue = "health";
   constructor(public popoverController: PopoverController) { }
@@ -15,7 +42,6 @@ export class PlaceDetailsPage implements OnInit {
   ngOnInit() {
   }
   async showUserScores() {
-    console.log("ffff");
 
     const popover = await this.popoverController.create({
       component: PlaceHealthDetailsComponent,
@@ -24,4 +50,76 @@ export class PlaceDetailsPage implements OnInit {
     await popover.present();
   }
 
+
+  getMaskAverage() {
+    let tmp = 0;
+    if (this.place?.placeHistory) {
+      for (let i = 0; i < this.place.placeHistory?.length; i++) {
+        tmp += this.place.placeHistory[i].mask;
+      }
+      tmp /= this.place.placeHistory.length;
+      return tmp;
+    }
+  }
+
+  getGloveAverage() {
+    let tmp = 0;
+    if (this.place?.placeHistory) {
+      for (let i = 0; i < this.place.placeHistory?.length; i++) {
+        tmp += this.place.placeHistory[i].glove;
+      }
+      tmp /= this.place.placeHistory.length;
+      return tmp;
+    }
+  }
+
+  getSanitizerAverage() {
+    let tmp = 0;
+    if (this.place?.placeHistory) {
+      for (let i = 0; i < this.place.placeHistory?.length; i++) {
+        tmp += this.place.placeHistory[i].sanitizer;
+      }
+      tmp /= this.place.placeHistory.length;
+      return tmp;
+    }
+  }
+
+  getAirConditioningAverage() {
+    let tmp = 0;
+    if (this.place?.placeHistory) {
+      for (let i = 0; i < this.place.placeHistory?.length; i++) {
+        tmp += this.place.placeHistory[i].airConditioning;
+      }
+      tmp /= this.place.placeHistory.length;
+      return tmp;
+    }
+  }
+
+  getSocialDistandingAverage() {
+    let tmp = 0;
+    if (this.place?.placeHistory) {
+      for (let i = 0; i < this.place.placeHistory?.length; i++) {
+        tmp += this.place.placeHistory[i].socialDistanding;
+      }
+      tmp /= this.place.placeHistory.length;
+      return tmp;
+    }
+  }
+
+  getColorBasedOnAverage(avg: number) {
+    if (avg >= 80) {
+      return 'success';
+    }
+    else if (avg >= 50) {
+      return 'warning'
+    }
+    else {
+      return 'danger'
+    }
+
+  }
+
+  getHistoriesWithComment() {
+    return this.place.placeHistory?.filter(x => x.comment);
+  }
 }
--- src/app/shared-components/place-health-details/place-health-details.component.ts
+++ src/app/shared-components/place-health-details/place-health-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlaceHistory } from 'src/app/interfaces/place-history';
 
 @Component({
   selector: 'app-place-health-details',
@@ -7,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceHealthDetailsComponent implements OnInit {
 
+  history: PlaceHistory = {
+    id: 1, mask: 86, sanitizer: 78, airConditioning: 88, socialDistanding: 60
+  };
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  getAverage() {
+    let tmp = 0;
+    tmp += (this.history.mask + this.history.sanitizer + this.history.airConditioning + this.history.socialDistanding);
+    return tmp / 4;
+  }
 
 }
