b1fe874272cf031ed2bc4bced1b120ff0fd93a8a
h.sinapour <h.sinapour@irisaco.com>
2020-10-04 09:14:41 +0330

    create services of config ,auth,account

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { ConfigService } from './config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  authRoute = "profiles"
   loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false)
-  constructor() { }
+  constructor(private configService: ConfigService, private http: HttpClient) { }
+
+  login(mobile) {
+    return this.http.post(`${this.configService.root}/${this.authRoute}/register?mobile=${mobile}`, {})
+  }
 }
